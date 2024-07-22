311fadd2140e3a768fee04e9db926e418807e3df
siva6 <siva6@cj.net>
2024-07-22 14:43:42 +0900

    add free badge

--- js/4_060.js
+++ js/4_060.js
@@ -9,6 +9,19 @@ const add_item_to_cart = (name, price) => {
 	calc_cart_total();
 };
 
+const update_shipping_icons = () => {
+	const buy_buttons = get_buy_buttons_dom();
+	for(var i = 0; i < buy_buttons.length; i++) {
+		const button = buy_buttons[i];
+		const item = button.item;
+		if(item.price + shopping_cart_total >= 20) {
+			button.show_free_shipping_icon();
+		}else {
+			button.hide_free_shipping_icon();
+		}
+	}
+};
+
 const calc_cart_total = () => {
 	shopping_cart_total = 0;
 	for(var i = 0; i < shopping_cart.length; i++) {
@@ -16,4 +29,5 @@ const calc_cart_total = () => {
 		shopping_cart_total += item.price;
 	}
 	set_cart_total_dom();
+	update_shipping_icons();
 };
--- js/default.js
+++ js/default.js
@@ -2,22 +2,22 @@ const _goods = [
 	{
 		img: 'https://image.tving.com/ntgs/contents/CTC/caim/CAIM2100/ko/20231221/0455/M000286841.jpg/dims/resize/F_webp,400',
 		name: '헝거게임',
-		price: 20
+		price: 8
 	},
 	{
 		img: 'https://image.tving.com/ntgs/contents/CTC/caip/CAIP0900/ko/20231218/1103/P001310647.jpg/dims/resize/F_webp,400',
 		name: '성균관 스캔들',
-		price: 37
+		price: 12
 	},
 	{
 		img: 'https://image.tving.com/ntgs/contents/CTC/caip/CAIP0900/ko/20240423/0600/P001755909.jpg/dims/resize/F_webp,400',
 		name: '귀멸의 칼날',
-		price: 15
+		price: 5
 	},
 	{
 		img: 'https://image.tving.com/ntgs/contents/CTC/caim/CAIM1150/ko/20240513/0232/M000369966.jpg/dims/resize/F_webp,400',
 		name: '라라랜드',
-		price: 19
+		price: 4
 	}
 ];
 
@@ -28,9 +28,36 @@ const makeItem = item => {
 		<img class="w-full" src="${item.img}" alt="${item.name}">
 		<div class="text-blue-500">${item.name}</div>
 		<div class="text-red-400">${item.price}</div>
-		<button onclick="add_item_to_cart('${item.name}', ${item.price})" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">장바구니</button>
+		<button onclick="add_item_to_cart('${item.name}', ${item.price})" class="cart-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+			장바구니
+		</button>
 	`;
 	return itemDom;
+};
+
+const makeBadge = () => {
+	const badge = document.createElement('div');
+	badge.classList.add('inline-flex', 'items-center', 'justify-center', 'w-8', 'h-8', 'text-xs', 'text-yellow', 'bg-red-500', 'border-2', 'border-white', 'rounded-full', '-top-2', '-end-2', 'dark:border-gray-900');
+	badge.innerText = 'free';
+	return badge;
+};
+
+const set_free = (button, show) => {
+	button.innerText = '장바구니';
+	if(show) {
+		button.appendChild(makeBadge());
+	}
+};
+
+const get_buy_buttons_dom = () => {
+	const buttons = document.querySelectorAll('.cart-btn');
+	return _goods.map((item, idx) => {
+		return {
+			item,
+			show_free_shipping_icon: () => set_free(buttons[idx], true),
+			hide_free_shipping_icon: () => set_free(buttons[idx], false)
+		};
+	});
 };
 
 /////////////////////////////////////////////////////////////////////////////////
