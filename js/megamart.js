a277b09b90a512a27e172ed526ffbdc221dcd860
siva6 <siva6@cj.net>
2024-07-23 15:27:09 +0900

    calc_total 추출 및 계산화

--- js/megamart.js
+++ js/megamart.js
@@ -26,12 +26,17 @@ const update_tax_dom = () => {
 	set_tax_dom(shopping_cart_total * 0.10);
 };
 
-const calc_cart_total = () => {
-	shopping_cart_total = 0;
-	for(var i = 0; i < shopping_cart.length; i++) {
-		var item = shopping_cart[i];
-		shopping_cart_total += item.price;
+const calc_total = cart => {
+	let	total = 0;
+	for (var i = 0; i < cart.length; i++) {
+		var item = cart[i];
+		total += item.price;
 	}
+	return total;
+}
+
+const calc_cart_total = () => {
+	shopping_cart_total = calc_total(shopping_cart);
 	set_cart_total_dom();
 	update_shipping_icons();
 	update_tax_dom();
